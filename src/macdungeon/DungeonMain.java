97e3a859ba31ee47983c16eb475fb1ff3765fd3c
Jarod <null4rs@gmail.com>
2013-02-24 11:55:24 -0600

    Added basis for entering a dungeon and for selecting floors

--- src/macdungeon/DungeonMain.java
+++ src/macdungeon/DungeonMain.java
@@ -4,7 +4,12 @@ import java.awt.Graphics;
 
 import org.powerbot.core.event.listeners.PaintListener;
 import org.powerbot.core.script.ActiveScript;
+import org.powerbot.core.script.job.state.Node;
+import org.powerbot.game.api.Manifest;
+import org.powerbot.game.api.methods.node.SceneEntities;
+import org.powerbot.game.api.wrappers.node.SceneObject;
 
+@Manifest(authors = { "Mac_Daddy" }, description = "Suck it iDungeon", name = "MacDungeoneering")
 public class DungeonMain extends ActiveScript implements PaintListener {
 
 	@Override
@@ -14,9 +19,25 @@ public class DungeonMain extends ActiveScript implements PaintListener {
 	}
 	
 	@Override
-	public void onRepaint(Graphics arg0) {
-		// TODO Auto-generated method stub
+	public void onRepaint(Graphics g) {
+		if (so != null)
+			so.draw(g);
+	}
+	
+	SceneObject so;
+	
+	class EnterDungeon extends Node {
+		
+		@Override
+		public boolean activate() {
+			so = SceneEntities.getNearest(Constants.DUNGEON_ENTRANCE);
+			
+			return so != null && so.isOnScreen();
+		}
+
+		@Override
+		public void execute() {
+		}
 		
 	}
-
 }

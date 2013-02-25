e4ffff67f0a6aac3b524b7e482bca9011e039d4e
Jarod <null4rs@gmail.com>
2013-02-24 21:38:19 -0600

    Added everything

---
+++ src/TestConstant.java
@@ -0,0 +1,12 @@
+
+public class TestConstant {
+
+	public static void main(String[] args) {
+		int a = 11940;
+		int b = 11984;
+		for (int i = a; i <= b; i++)
+			System.out.print(i + ",");
+
+	}
+
+}
